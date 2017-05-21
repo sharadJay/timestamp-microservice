36b88fcdd7327fedeb206ce7403982e2d6cf4195
Sharad Jain <shjain@expedia.com>
2017-05-21 09:25:47 +0530

    timestamp microservice

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
+const port = process.env.PORT || 8080;
+const time = require("moment");
+
+app.get("/:time", (request, response)=> {
+
+    if (isNaN(request.params.time)) {
+        let jsDate = new Date(request.params.time);
+        let date = time(jsDate).format("MMMM D YYYY");
+        response.send({
+            "naturalDate": date,
+            "unix": +jsDate
+        });
+    } else {
+        response.send({
+            "unix": request.params.time,
+            "naturalDate": time.unix(parseInt(request.params.time)).format("MMMM D YYYY")
+        })
+    }
+});
+
+app.listen(port, () => console.log(`listening on ${port}`));
